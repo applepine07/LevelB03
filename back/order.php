464aa9870b033a0f0fe31473cd5b0452dd94111a
applepine07 <mrchildrenmissadult@hotmail.com>
2022-03-27 22:49:40 +0800

    要開始步驟10

--- back/order.php
+++ back/order.php
@@ -1,1 +1,86 @@
-訂購
+<h3 class="ct">訂單清單</h3>
+<style>
+
+.field,
+.row{
+    display:flex;
+}
+.field span,
+.row span{
+    flex:1;
+    background:#eee;
+    margin:0 1px;
+    text-align: center;
+}
+.row span{
+    background:white;
+}
+.list{
+    height:400px;
+    overflow:auto;
+}
+</style>
+<div>
+    快速刪除：
+    <input type="radio" name="type" value="date">
+    依日期
+    <input type="text" name="date" id="date">
+
+    <input type="radio" name="type" value="movie">
+    依電影
+    <select name="movie" id="movie"></select>
+    <button onclick="qdel()">刪除</button>
+</div>
+<div class='field'>
+    <span>訂單編號</span><span>電影名稱</span><span>日期</span><span>場次時間</span><span>訂購數量</span><span>訂購位置</span><span>操作</span>
+</div>
+<div class='list'>
+<?php
+$rows=$Ord->all(" Order by `no` DESC");
+
+foreach($rows as $row){
+    echo "<div class='row'>";
+    echo "  <span>{$row['no']}</span>";
+    echo "  <span>{$row['movie']}</span>";
+    echo "  <span>{$row['date']}</span>";
+    echo "  <span>{$row['session']}</span>";
+    echo "  <span>{$row['qt']}</span>";
+    echo "  <span>";
+        $seats=unserialize($row['seat']);
+        foreach($seats as $seat){
+            echo  (floor($seat/5)+1). "排".($seat%5 +1)."號";
+            echo "<br>";
+         }
+    echo "</span>";
+    echo "  <span><button onclick='del(&#39;ord&#39;,{$row['id']})'>刪除</button></span>";
+    echo "</div>";
+    echo "<hr>";
+}
+
+?>
+
+</div>
+
+<script>
+    $("#movie").load("api/get_ord_movies.php")
+
+
+function qdel(){
+    let type=$("input[name='type']:checked").val()
+    let target
+    switch(type){
+        case 'date':
+            target=$('#date').val()
+        break;
+        case 'movie':
+            target=$('#movie').val()
+        break;
+    }
+    let chk=confirm(`你確定要刪除${target}的所有訂單資料嗎?`)
+    if(chk){
+        $.post("api/qdel.php",{type,target},()=>{
+            location.reload()
+        })
+    }
+}
+</script>
--- front/order.php
+++ front/order.php
@@ -1,1 +1,97 @@
-線上訂票
+<h3 class='ct'>線上訂票</h3>
+<style>
+    #order{
+        width:50%;
+        margin:auto;
+    }
+    .row{
+        display:flex;
+        width:100%;
+    }
+    .row .first{
+        width:15%;
+        text-align:right;
+    }
+    .row .sec{
+        width:85%;
+        text-align:left;
+    }
+    .sec select{
+        width:100%;
+    }
+
+</style>
+<div id="order">
+<div class="row">
+    <div class="first">電影：</div>
+    <div class="sec"><select name="movie" id="movie"></select></div>
+</div>
+<div class="row">
+    <div class="first">日期：</div>
+    <div class="sec"><select name="date" id="date"></select></div>
+</div>
+<div class="row">
+    <div class="first">場次：</div>
+    <div class="sec"><select name="session" id="session"></select></div>
+</div>
+<div class="row">
+    <div class="ct" style="width:100%">
+        <button onclick="booking()">確定</button>
+        <button onclick="reset()">重置</button>
+    </div>
+    
+</div>
+</div>
+
+<div id="booking" style="display:none"></div>
+<script>
+let id=(new URL(location)).searchParams.get('id');
+getMovies(id)
+
+$("#movie").on("change",()=>{getDays()})
+$("#date").on("change",()=>{getSessions()})
+
+
+
+function booking(){
+    $("#order,#booking").toggle()
+
+    let order={id:$("#movie").val(),
+               date:$("#date").val(),
+               session:$("#session").val()}
+    $.get("api/booking.php",order,(booking)=>{
+        $("#booking").html(booking)
+    })
+}
+
+function reset(){
+    getMovies(id)
+}
+function prev(){
+    $("#order,#booking").toggle()
+    $("#booking").html("");
+}
+
+function getMovies(id){
+    $.get("api/get_movies.php",{id},(movies)=>{
+        $("#movie").html(movies)
+        getDays()
+    })
+}
+
+function getDays(){
+    let id=$("#movie").val();
+    $.get("api/get_days.php",{id},(days)=>{
+        $("#date").html(days)
+        getSessions();
+    })
+}
+
+function getSessions(){
+    let id=$("#movie").val();
+    let date=$("#date").val();
+    $.get("api/get_sessions.php",{id,date},(sessions)=>{
+        $("#session").html(sessions)
+    })
+}
+</script>
