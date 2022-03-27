db1a84a0ac0f9bbe35e5b7741611a862c4e377e6
applepine07 <mrchildrenmissadult@hotmail.com>
2022-03-27 14:13:20 +0800

    步驟四ok

--- api/edit_poster.php
+++ api/edit_poster.php
@@ -1,15 +1,19 @@
 <?php
 include_once "../base.php";
+// 以傳過來的id為主去比對
 foreach ($_POST['id'] as $key => $id) {
+    // 有在del陣列裡就刪除
     if (isset($_POST['del']) && in_array($id, $_POST['del'])) {
         $Poster->del($id);
     } else {
+        // 沒有的話就撈出來更新後
         $po = $Poster->find($id);
         $po['name']=$_POST['name'][$key];
         $po['ani']=$_POST['ani'][$key];
         // $po['rank']=$_POST['rank'][$key];
         $po['sh']=(isset($_POST['sh']) && in_array($id,$_POST['sh']))?1:0;
 
+        // 再存回去
         $Poster->save($po);
     }
 }
